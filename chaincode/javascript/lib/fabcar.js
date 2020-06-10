8884a8411686c2b85e1abe816471ec89477caa0f
Thao Truong <thaotruong199826@gmail.com>
2020-06-10 07:33:47 -0700

    getid

--- chaincode/javascript/lib/fabcar.js
+++ chaincode/javascript/lib/fabcar.js
@@ -5,19 +5,19 @@ const ClientIdentity = require('fabric-shim').ClientIdentity;
 
 class QuanLyDiem extends Contract {
 	
-	//async getIdentity(ctx) { 
+	async getIdentity(ctx) { 
 		/*let identity = null
 		return identity*/
-		//let cid = new ClientIdentity(ctx)
+		let cid = new ClientIdentity(ctx.stub)
 		// if !cid.firstName:
 		// 	throw 
 
 		// if(cid.assertAttributeValue('firstName','Thao')){
 		// 	throw new Error('Not a valid user')
 		// }
-		//identity = cid.firstName
-		//return identity
-	//}
+		let id = cid.getID()
+		return id
+	}
 
 	async khoiTao(ctx){
 		const obj_sv = 
@@ -80,14 +80,14 @@ class QuanLyDiem extends Contract {
 	async suaDiem(ctx, mssv, maLopHocPhan,diemmoi){ // giang vien se them diem/hoac sua diem hoc maLopHocPhan cua sinh vien co mssv voi diem tuong ung 
 		// kiem tra giangvien co the sua diem cua "maLopHocPhan" khong
 		// tien hanh sua diem cua sinh vien, va luu ten nguoi sua lai
-		//const identity = await getIdentity(ctx)
+		const identity = await this.getIdentity(ctx)
 		const sv = await ctx.stub.getState(mssv);
 		const svinfo = JSON.parse(sv.toString());
 		// lay ma hoc phan
 		const maNhom = svinfo.hocphan[maLopHocPhan]
 		// sua diem cu thanh diem moi
 		svinfo.hocphan[maLopHocPhan].diem = diemmoi;
-		//svinfo.hocphan[maLopHocPhan].choboi = identity; 
+		svinfo.hocphan[maLopHocPhan].choboi = identity; 
 		const result = await ctx.stub.putState(mssv, Buffer.from(JSON.stringify(svinfo)))
 		console.log(svinfo)
 	}
